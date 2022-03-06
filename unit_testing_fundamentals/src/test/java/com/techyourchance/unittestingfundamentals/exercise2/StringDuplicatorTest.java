dcefc1f459feae2399f0a0f709773e8f111190a2
Luis Lugo <luis.lugo@shockoe.com>
2022-03-06 14:17:07 -0600

    latest cod3

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicatorTest.java
@@ -1,5 +1,34 @@
 package com.techyourchance.unittestingfundamentals.exercise2;
 
-public class StringDuplicatorTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringDuplicatorTest {
+    StringDuplicator SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new StringDuplicator();
+    }
+
+    @Test
+    public void duplicate_emptyString_emptyStringReturned() {
+        String result = SUT.duplicate("");
+        assertThat(result, is(""));
+    }
+
+    @Test
+    public void duplicate_singleCharacter_sameCharacterTwice() {
+        String result = SUT.duplicate("a");
+        assertThat(result, is("aa"));
+    }
+
+    @Test
+    public void duplicate_longString_longStringDuplicated() {
+        String result = SUT.duplicate("Luis");
+        assertThat(result, is("LuisLuis"));
+    }
 }
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise3/IntervalsAdjacencyDetectorTest.java
@@ -1,5 +1,90 @@
 package com.techyourchance.unittestingfundamentals.exercise3;
 
-public class IntervalsAdjacencyDetectorTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
+import com.techyourchance.unittestingfundamentals.example3.Interval;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntervalsAdjacencyDetectorTest {
+    IntervalsAdjacencyDetector SUT;
+
+    @Before
+    public void setUp() throws Exception {
+        SUT = new IntervalsAdjacencyDetector();
+    }
+
+    // interval1 before interval2
+    @Test
+    public void isAdjacent_interval1BeforeInterval2_returnFalse() {
+        Interval interval1 = new Interval(1, 10);
+        Interval interval2 = new Interval(11, 20);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
+
+    // interval1 starts before interval2 starts
+    @Test
+    public void isAdjacent_interval1StartsBeforeInterval2Starts_returnFalse() {
+        Interval interval1 = new Interval(1, 10);
+        Interval interval2 = new Interval(2, 11);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
+
+    // interval1 starts after interval2 starts
+    @Test
+    public void isAdjacent_interval1StartsAfterInterval2Starts_returnFalse() {
+        Interval interval1 = new Interval(3, 10);
+        Interval interval2 = new Interval(2, 11);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
+
+    // interval1 ends after interval2 ends
+    @Test
+    public void isAdjacent_interval1EndsAfterInterval2Ends_returnFalse() {
+        Interval interval1 = new Interval(1, 12);
+        Interval interval2 = new Interval(2, 11);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
+
+    // interval1 starts adjacently with interval2 ends
+    @Test
+    public void isAdjacent_interval1StartsAdjacentlyWithInterval2Ends_returnTrue() {
+        Interval interval1 = new Interval(12, 17);
+        Interval interval2 = new Interval(2, 12);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(true));
+    }
+
+    // interval1 ends adjacently with interval2 starts
+    @Test
+    public void isAdjacent_interval1EndsAdjacentlyWithInterval2Starts_returnTrue() {
+        Interval interval1 = new Interval(1, 17);
+        Interval interval2 = new Interval(17, 20);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(true));
+    }
+
+    // interval1 after interval2
+    @Test
+    public void isAdjacent_interval1AfterInterval2_returnTrue() {
+        Interval interval1 = new Interval(13, 17);
+        Interval interval2 = new Interval(2, 12);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
+
+    // interval1 and interval2 are equal
+    @Test
+    public void isAdjacent_interval1AndInterval2Equal_returnFalse() {
+        Interval interval1 = new Interval(1, 12);
+        Interval interval2 = new Interval(1, 12);
+        Boolean result = SUT.isAdjacent(interval1, interval2);
+        assertThat(result, is(false));
+    }
 }
